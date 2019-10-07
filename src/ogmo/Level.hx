44e8de8dc98bc114ba6d97b68ddfcb3114fa2a29
Sébastien Bénard <github@deepnight.net>
2019-10-07 17:09:47 +0200

    Added level values getters

--- src/ogmo/Level.hx
+++ src/ogmo/Level.hx
@@ -16,6 +16,8 @@ class Level {
 	public var offX: Int;
 	public var offY : Int;
 
+	var values : Dynamic;
+
 	public function new(p:Project, file:hxd.res.Resource, json:Dynamic) {
 		project = p;
 		name = file.name;
@@ -25,6 +27,9 @@ class Level {
 		pxHei = json.height;
 		offX = json.offsetX;
 		offY = json.offsetY;
+
+		values = json.values==null ? {} : json.values;
+
 		var jsonLayers : Array<Dynamic> = cast json.layers;
 		for(json in jsonLayers) {
 			var l = new Layer(this, json);
@@ -35,4 +40,45 @@ class Level {
 		layersReversed = layers.copy();
 		layersReversed.reverse();
 	}
+
+
+
+	public inline function has(v:String) return Reflect.hasField(values,v);
+	public inline function get(v:String) return Reflect.field(values,v);
+
+	public inline function getStr(v:String, ?def:String="") {
+		return has(v) ? get(v): def;
+	}
+
+	public inline function getBool(v:String, ?def=false) {
+		return has(v) ? get(v)=="true" : def;
+	}
+
+	public inline function getInt(v:String, ?def=0) {
+		var out = Std.parseInt( get(v) );
+		return has(v) && out!=null ? out : def;
+	}
+
+	public inline function getFloat(v:String, ?def=0.) {
+		var out = Std.parseFloat( get(v) );
+		return has(v) && !Math.isNaN(out) && Math.isFinite(out) ? out : def;
+	}
+
+	public inline function getColor(v:String, ?def=0x0) {
+		if( !has(v) )
+			return def;
+		var out = dn.Color.hexToInt( get(v).substr(0,7) );
+		return has(v) && out!=null ? out : def;
+	}
+
+	public inline function getEnum<T>(v:String, e:Enum<T>) : T {
+		if( !has(v) )
+			throw name+" has no enum field "+v;
+
+		try return e.createByName(get(v))
+		catch(err:Dynamic) {
+			throw "Enum value "+get(v)+" found in "+name+"."+v+" doesn't exist in source code!";
+		}
+	}
+
 }
