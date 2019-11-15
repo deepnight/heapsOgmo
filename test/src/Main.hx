2458d861ddbf2f68ef08788b1bfc18058a3ee8e9
Sébastien Bénard <fork@deepnight.net>
2019-11-15 10:42:40 +0100

    Added top-down test Renamed test levels

--- test/src/Main.hx
+++ test/src/Main.hx
@@ -2,6 +2,7 @@ import hxd.Key;
 
 class Main extends dn.Process {
 	public static var ME : Main;
+	var wrapper : h2d.Object;
 
 	public function new(s:h2d.Scene) {
 		super();
@@ -26,14 +27,21 @@ class Main extends dn.Process {
 		h2d.Console.HIDE_LOG_TIMEOUT = 99999;
 		dn.Lib.redirectTracesToH2dConsole(c);
 
-		// Start
-		var ogmoProject = new ogmo.Project(hxd.Res.map.project, false);
+		wrapper = new h2d.Object(root);
+		wrapper.scale(4);
+		hxd.Res.map.topDown_json.watch( function() renderMap() );
+		renderMap();
+	}
 
-		for(level in ogmoProject.levels)
-		for(layer in level.layers) {
+	function renderMap() {
+		wrapper.removeChildren();
+
+		var ogmoProject = new ogmo.Project(hxd.Res.map.topDown_ogmo, false);
+		var level = ogmoProject.getLevelByName("topDown");
+
+		for(layer in level.layersReversed) {
 			var obj = layer.render();
-			root.addChild(obj);
-			obj.scale(3);
+			wrapper.addChild(obj);
 		}
 	}
 }
